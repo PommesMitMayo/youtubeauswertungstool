87b5ca63a8b957a2a7c1a41f3e1dc8c369622c6a
Daniel Vossen <dvossen@uni-koblenz.de>
2021-05-29 18:32:03 +0200

    interactive and x occurrence fix

--- main.py
+++ main.py
@@ -53,16 +53,18 @@ def generate_data_sets(x, y, results):
             if x_val not in data_sets:
                 x_vals[x_val] = 0
                 data_sets[x_val] = {}
-            x_vals[x_val] += 1
+            x_has_at_least_one_y = False
             for y_val in result[y].split(';'):
                 y_val = y_val.split(' ')[0] if y == '25. Alter' else y_val
                 if y_val and y_val not in y_vals:
                     y_vals[y_val] = 0
                 if x_val and y_val:
+                    x_has_at_least_one_y = True
                     if y_val not in data_sets[x_val]:
                         data_sets[x_val][y_val] = 0
                     data_sets[x_val][y_val] += 1
                     y_vals[y_val] += 1
+            if x_has_at_least_one_y: x_vals[x_val] += 1
 
     return {k: v for k, v in data_sets.items() if v}, x_vals, y_vals
 
@@ -107,7 +109,7 @@ def calculate_normalized(data_sets, xs_occurrence, use_max_percentage):
                     data_sets[data_set_key][y_key], data_sets[data_set_key][y_key] * maximum / data_set_max)
 
 
-def plot_charts(csv_file, x_axis_id, y_axis_id, *options):
+def plot_charts(csv_file, x_axis_id, y_axis_id):
     pp = pprint.PrettyPrinter(indent=4, width=400, sort_dicts=False)
 
     results = list(create_dict(csv_file).values())
@@ -115,47 +117,52 @@ def plot_charts(csv_file, x_axis_id, y_axis_id, *options):
     key_y_axis = get_key(y_axis_id, results[0])
     data_sets, xs_occurrence, unsorted_ys = generate_data_sets(key_x_axis, key_y_axis, results)
     pp.pprint(data_sets)
+    calculate_normalized(data_sets, xs_occurrence, True)
+    x_locations = get_x_locations(data_sets, True)
 
     xs = sorted(list(data_sets), key=custom_sort_value)
-    if 'sort_y' in options:
-        ys = sorted(list(unsorted_ys), key=lambda y: unsorted_ys[y])
-    else:
-        ys = sorted(list(unsorted_ys), key=custom_sort_value)
-    x_locations = get_x_locations(data_sets, 'norm_percentage' in options)
-    bin_edges = list(range(len(ys)))
-    calculate_normalized(data_sets, xs_occurrence, 'norm_percentage' in options)
+    for sort_y in [False, True]:
+        if sort_y:
+            ys = sorted(list(unsorted_ys), key=lambda y: unsorted_ys[y])
+        else:
+            ys = sorted(list(unsorted_ys), key=custom_sort_value)
+        bin_edges = list(range(len(ys)))
 
-    px = 1 / plt.rcParams['figure.dpi']
-    fig, ax = plt.subplots(figsize=(1366 * px, 768 * px))
-    fig.canvas.manager.set_window_title('_'.join([x_axis_id, y_axis_id, *options]))
+        px = 1 / plt.rcParams['figure.dpi']
+        fig, ax = plt.subplots(figsize=(1366 * px, 768 * px))
+        fig.canvas.manager.set_window_title(
+            '_'.join([x_axis_id, y_axis_id, 'sorted_by_occurrence' if sort_y else 'custom_sorted']))
 
-    for x_location, data_set_key in zip(x_locations, xs):
-        binned_data = [data_sets[data_set_key][y][1] if y in data_sets[data_set_key] else 0 for y in ys]
-        binned_data_text = [
-            str(round(100 * data_sets[data_set_key][y][0] / xs_occurrence[data_set_key], 1)) + '% (' + str(
-                data_sets[data_set_key][y][0]) + '/' + str(xs_occurrence[data_set_key]) + ')' if y in data_sets[
-                data_set_key] else '' for y in ys]
-        lefts = [x_location - 0.5 * bd for bd in binned_data]
-        bars = ax.barh(bin_edges, binned_data, left=lefts, height=1)
-        for bar in bars:
-            ax.text(bar.get_x() + bar.get_width() / 2,
-                    bar.get_y() + .5,
-                    binned_data_text.pop(0),
-                    ha='center',
-                    va='center',
-                    weight='bold')
+        for x_location, data_set_key in zip(x_locations, xs):
+            binned_data = [data_sets[data_set_key][y][1] if y in data_sets[data_set_key] else 0 for y in ys]
+            binned_data_text = [
+                str(round(100 * data_sets[data_set_key][y][0] / xs_occurrence[data_set_key], 1)) + '% (' + str(
+                    data_sets[data_set_key][y][0]) + '/' + str(xs_occurrence[data_set_key]) + ')' if y in data_sets[
+                    data_set_key] else '' for y in ys]
+            lefts = [x_location - 0.5 * bd for bd in binned_data]
+            bars = ax.barh(bin_edges, binned_data, left=lefts, height=1)
+            for bar in bars:
+                ax.text(bar.get_x() + bar.get_width() / 2,
+                        bar.get_y() + .5,
+                        binned_data_text.pop(0),
+                        ha='center',
+                        va='center',
+                        weight='bold')
 
-    ax.set_xticks(x_locations)
-    ax.set_xticklabels(xs, wrap=True)
+        ax.set_xticks(x_locations)
+        ax.set_xticklabels(xs, wrap=True)
 
-    ax.set_yticks(bin_edges)
-    ax.set_yticklabels(ys, wrap=True)
+        ax.set_yticks(bin_edges)
+        ax.set_yticklabels(ys, wrap=True)
 
-    ax.set_xlabel(key_x_axis, wrap=True)
-    ax.set_ylabel(key_y_axis, wrap=True)
+        ax.set_xlabel(key_x_axis, wrap=True)
+        ax.set_ylabel(key_y_axis, wrap=True)
 
-    plt.tight_layout()
-    plt.savefig('_'.join([x_axis_id, y_axis_id, *options]) + '.png')
+        plt.tight_layout()
+        created_file_name = '_'.join(
+            [x_axis_id, y_axis_id, 'sorted_by_occurrence' if sort_y else 'custom_sorted']) + '.png'
+        plt.savefig(created_file_name)
+        print('Saved as ' + created_file_name)
 
 
 def print_all_words(csv_file, question_id):
@@ -185,10 +192,13 @@ def filter_csv(csv_file):
 
 
 if __name__ == '__main__':
-    if sys.argv[1] == 'plot':
-        plot_charts(*sys.argv[2:], 'norm_percentage')
-        plot_charts(*sys.argv[2:], 'sort_y', 'norm_percentage')
-    if sys.argv[1] == 'word_cloud':
-        print_all_words(*sys.argv[2:])
-    if sys.argv[1] == 'filter':
-        filter_csv(*sys.argv[2:])
+    operation = input('Choose operation (plot|word_cloud) --> ')
+    if operation == 'plot':
+        x_axis_id = input('Choose X-Axis question identifier --> ')
+        y_axis_id = input('Choose Y-Axis question identifier --> ')
+        plot_charts(sys.argv[1], x_axis_id, y_axis_id)
+    if operation == 'word_cloud':
+        identifier = input('Choose question identifier --> ')
+        print_all_words(sys.argv[1], identifier)
+    if operation == 'filter':
+        filter_csv(sys.argv[1])
